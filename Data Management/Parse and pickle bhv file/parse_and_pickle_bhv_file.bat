c2a8f78c6150c2ab53097564b31116ededdfa977
Ali Hadi Shenasa <38480296+AliShenasa@users.noreply.github.com>
2024-02-21 09:42:15 -0800

    Fix path to python script in batch files

--- Data Management/Parse and pickle bhv file/parse_and_pickle_bhv_file.bat
+++ Data Management/Parse and pickle bhv file/parse_and_pickle_bhv_file.bat
@@ -5,5 +5,5 @@ SET log_file="Z:\Data Management\Logs"
 SET folder="Z:\Live Fly Imaging data\fictrac"
 call %UserProfile%\Anaconda3\Scripts\activate.bat
 call conda activate gulp2p
-python parse_and_pickle_bhv_file.py > %log_file%
+python %~dp0\parse_and_pickle_bhv_file.py > %log_file%
 call conda deactivate
--- Data Management/connect_to_nas.bat
+++ Data Management/connect_to_nas.bat
@@ -2,6 +2,6 @@ REM Batch file to connect to NAS
 REM Scheduled to run on Lab Workstation startup
 
 if exist z:\ (
-    net use z: /delete
+    net use z: /delete /y
 )
 net use z: "\\TurnerEvans-NAS\Lab Storage"

a5a03d0f83bbbb51b487f166e3de2309d1261aba
Ali Hadi Shenasa <38480296+AliShenasa@users.noreply.github.com>
2024-02-21 10:04:11 -0800

    Fix spaces in path names

--- Data Management/Fix avi meteadata/fix_avi_metadata.bat
+++ Data Management/Fix avi meteadata/fix_avi_metadata.bat
@@ -3,6 +3,6 @@ REM Batch file to run fix_avi_metadata.py
 
 SET log_file="Z:\Data Management\Logs"
 SET folder="Z:\Live Fly Imaging data\fictrac"
-call %~dp0\..\activate_conda_env.bat avi_fix
-python %~dp0\fix_avi_metadata.py %folder% > %log_file%
+call "%~dp0\..\activate_conda_env.bat" avi_fix
+python "%~dp0\fix_avi_metadata.py" %folder% > %log_file%
 call conda deactivate
--- Data Management/activate_conda_env.bat
+++ Data Management/activate_conda_env.bat
@@ -5,7 +5,7 @@ rem https://gist.github.com/maximlt/531419545b039fa33f8845e5bc92edd6
 rem Define here the path to your conda installation
 set CONDAPATH=%UserProfile%\Anaconda3
 rem Get env name from the batch argument
-set ENVNAME=%1
+set ENVNAME="%1"
 
 rem The following command activates the base environment.
 rem call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
@@ -13,4 +13,4 @@ if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\
 
 rem Activate the conda environment
 rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
-call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
+call "%CONDAPATH%\Scripts\activate.bat" %ENVPATH%

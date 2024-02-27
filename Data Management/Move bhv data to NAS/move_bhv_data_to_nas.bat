2dfb76c1cab8e47d52c027c2a2f05e37fdb564dd
Ali Hadi Shenasa <38480296+AliShenasa@users.noreply.github.com>
2024-02-27 14:26:43 -0800

    Fix NAS paths

--- Data Management/Move bhv data to NAS/move_bhv_data_to_nas.bat
+++ Data Management/Move bhv data to NAS/move_bhv_data_to_nas.bat
@@ -2,7 +2,7 @@
 REM Batch file to transfer behavioral data to the NAS
 
 SET src="C:\Users\exx\AppData\LocalLow\Default company\Fly Virtual Reality - 3D Rotating test world"
-SET dst="Z:\Live Fly Imaging data\unity\raw data\Backups"
+SET dst="\\TurnerEvans-NAS\Lab Storage\Live Fly Imaging data\unity\raw data\Backups"
 SET exclude="SessionParameters.json"
 
 REM Append time and date to filename
@@ -12,7 +12,7 @@ set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
-SET log_file="Z:\Data Management\Logs\move_bhv_data_to_nas_%fullstamp%.log"
+SET log_file="\\TurnerEvans-NAS\Lab Storage\Data Management\Logs\move_bhv_data_to_nas_%fullstamp%.log"
 
 REM Move files from source to destination and create a log file
-robocopy %src% %dst% *.json /S /E /DCOPY:DAT /MOV /IS /IM /IT /XF %exclude% /LOG+:%log_file% /NP
+robocopy %src% %dst% *.json /S /E /DCOPY:DAT /MOV /IS /IM /IT /XF %exclude% /LOG:%log_file% /NP

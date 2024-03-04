7bf0dd4bdecf3d912563876a30b9810dc2a871a5
Ali Hadi Shenasa <38480296+AliShenasa@users.noreply.github.com>
2024-03-04 14:51:25 -0800

    Fix NAS path

--- Data Management/Parse and pickle bhv file/parse_and_pickle_bhv_file.bat
+++ Data Management/Parse and pickle bhv file/parse_and_pickle_bhv_file.bat
@@ -4,7 +4,7 @@ REM Run in windows task scheduler with:
 REM Program: Wscript
 REM Args: "C:\Users\remoteuser\Documents\GitHub\lab-tools\Data Management\launchquiet.vbs" "C:\Users\remoteuser\Documents\GitHub\lab-tools\Data Management\Parse and pickle bhv file\parse_and_pickle_bhv_file.bat"
 
-SET folder="Z:\Live Fly Imaging data\fictrac"
+SET folder="\\TurnerEvans-NAS\Lab Storage\Live Fly Imaging data\fictrac"
 REM Create log file name
 call "%~dp0\..\create_log_file_name.bat" parse_and_pickle_bhv_file
 
